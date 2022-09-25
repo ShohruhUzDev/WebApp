5ef0d5ec58e90630e31e724abf1e9a9200a93750
ShohruhUzDev <shohruh7464784@mail.ru>
2022-09-25 10:04:50 +0500

    add dbcontext

---
+++ WebApp/WebApp/Context/UserContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.Context
+{
+    public class UserContext:DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options):base(options)
+        {
+
+        }
+
+        DbSet<UserContext> Users { get; set; }  
+    }
+}
