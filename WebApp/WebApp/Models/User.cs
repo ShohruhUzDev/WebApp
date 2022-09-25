467f190e02a3d775503e666bcf4fe591d0998009
ShohruhUzDev <shohruh7464784@mail.ru>
2022-09-25 10:54:18 +0500

    add new model

--- WebApp/WebApp/Models/User.cs
+++ WebApp/WebApp/Models/User.cs
@@ -1,9 +1,20 @@
-﻿namespace WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
-         
+        
+        [Required]
+        public string? Name { get; set; }
+        
+        [Required]
+        public string? Email { get; set; }
+        public DateTime LastLoginTime { get; set; }
+        public DateTime RegistrationTime { get; set; }
+        public bool Status { get; set; }    
+
     }
 }
