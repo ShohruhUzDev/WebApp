eb9ee01ab6e3638fec5174e95383c3dc11a90234
ShohruhUzDev <shohruh7464784@mail.ru>
2022-09-25 10:10:50 +0500

    add dbcontext to programm.cs file

--- WebApp/WebApp/Program.cs
+++ WebApp/WebApp/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using WebApp.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<UserContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlserverConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
