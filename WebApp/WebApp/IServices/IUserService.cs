aae9a5a3d4471a767a4b48edbbf094bae051d104
ShohruhUzDev <shohruh7464784@mail.ru>
2022-09-25 09:56:09 +0500

    Add Interfesis

---
+++ WebApp/WebApp/IServices/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using WebApp.Models;
+
+namespace WebApp.IServices
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User> GetbyIdUserAsync(Guid id);
+        Task CreateUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task DeleteUser(Guid id);
+    }
+}
