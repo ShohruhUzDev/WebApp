b8031fd7e25fbe09749855ffa5d2654445481408
ShohruhUzDev <shohruh7464784@mail.ru>
2023-01-09 19:00:22 +0500

    add  changes

--- Malika_App/Malika/UserControls/if_control.xaml.cs
+++ Malika_App/Malika/UserControls/if_control.xaml.cs
@@ -20,9 +20,33 @@ namespace Malika.UserControls
     /// </summary>
     public partial class if_control : UserControl
     {
+
+
+        public string SelectedAtribut { get; set; } = "";
+        public string SelectedQiymat { get; set; } = "";
         public if_control()
         {
             InitializeComponent();
         }
+
+        //public void atribut_cbx1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    SelectedAtr();
+        //}
+
+        public void SelectedAtr()
+        {
+            MessageBox.Show( atribut_cbx1.SelectedItem.ToString()!);
+        }
+
+        //public void qiymat_cbx1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    SelectedQiy();
+        //}
+        public void SelectedQiy()
+        {
+            MessageBox.Show( qiymat_cbx1.SelectedItem.ToString()!);
+        }
+
     }
 }
--- Malika_App/Malika/Views/Export.xaml.cs
+++ Malika_App/Malika/Views/Export.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace Malika.Views
 {
@@ -20,9 +21,11 @@ namespace Malika.Views
     /// </summary>
     public partial class Export : Window
     {
-
         public List<string> Atributs { get; set; }=new List<string>();  
+        public List<string> SelectedAtributs { get; set; }=new List<string>();
         public List<string> Qiymats { get; set; }=new List<string>();   
+        public List<string> SelectedQiymats { get; set; }=new List<string>();
+        public bool first_selected=false;
         public Export()
         {
             InitializeComponent();
@@ -32,7 +35,7 @@ namespace Malika.Views
         {
             atributlar_lstbx.Items.Add(atribut_txt.Text);
             Atributs.Add(atribut_txt.Text);
-          //  atributlar_lstbx.ItemsSource = Atributs;
+         
             atribut_txt.Clear();
         }
 
@@ -40,16 +43,76 @@ namespace Malika.Views
         {
             qiymatlar_lstbx.Items.Add(qiymat_txt.Text);
             Qiymats.Add(qiymat_txt.Text);
-           // qiymatlar_lstbx.ItemsSource= Qiymats;
+           
             qiymat_txt.Clear();
         }
 
         private void add_btn_Click(object sender, RoutedEventArgs e)
         {
-            if_control if_Control = new if_control();
-            oyna.Children.Add(if_Control);
-        }
+           
 
+
+
+
+            try
+            {
+
+                if_control if_Control = new if_control();
+
+
+                if_Control.qiymat_cbx1.SelectionChanged +=new SelectionChangedEventHandler(OnQiymatSelected);
+                if_Control.atribut_cbx1.SelectionChanged+=new SelectionChangedEventHandler(OnAtributSelected);
+             
+                oyna.Children.Add(if_Control);
+
+                //if(first_selected)
+                //{
+                //    Qiymats.Remove(if_Control.SelectedQiymat);
+                //    Atributs.Remove(if_Control.SelectedAtribut);
+
+                //}
+
+
+
+
+
+                foreach (var i in Atributs)
+                {
+                   
+                        if_Control.atribut_cbx1.Items.Add(i);
+                    
+                }
+                foreach (var i in Qiymats)
+                {
+                   
+                        if_Control.qiymat_cbx1.Items.Add(i);
+                    
+                }
+
+
+
+            }
+           
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+        }
+        public void OnAtributSelected(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            Atributs.Remove(comboBox.SelectedItem.ToString()!);
+
+        }
+        public void OnQiymatSelected(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+            Qiymats.Remove(comboBox.SelectedItem.ToString()!);
+
+        }
         private void delete_btn_Click(object sender, RoutedEventArgs e)
         {
                  if(qiymatlar_lstbx.SelectedIndex!=-1)
@@ -80,6 +143,28 @@ namespace Malika.Views
         private void save_btn_Click(object sender, RoutedEventArgs e)
         {
 
+
+            foreach( var i  in Atributs)
+            {
+                atribut_cbx.Items.Add(i);
+            }
+
+            foreach (var i in Qiymats)
+            {
+                qiymat_cbx.Items.Add(i);
+            }
+        }
+
+        private void atribut_cbx_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           Atributs.Remove(atribut_cbx.SelectedItem.ToString()!);
+            first_selected = true;
+        }
+
+        private void qiymat_cbx_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           Qiymats.Remove(qiymat_cbx.SelectedItem.ToString()!); 
+            first_selected=true;
         }
     }
 }
